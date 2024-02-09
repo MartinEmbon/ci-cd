dd77922f5b3ccc119fe5b9db21426807bacb1add
Martin <martinembon@hotmail.com>
2024-02-09 15:50:43 -0300

    first

--- test_app.py
+++ test_app.py
@@ -19,7 +19,7 @@ class Tests(unittest.TestCase):
         res = self.app.get('/null')
         assert res.status == '404 NOT FOUND'
 
-print("hello")
+print("hello1")
 
 if __name__ == "__main__":
     unittest.main()
